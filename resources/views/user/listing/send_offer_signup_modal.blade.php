d4627416987e755e3dc0f76be146075d2efdb060
S. M. Abrar Jahin <abrarjahin@live.com>
2016-05-15 21:10:39 +0600

    listing page tab fixed

--- resources/views/user/listing/send_offer_signup_modal.blade.php
+++ resources/views/user/listing/send_offer_signup_modal.blade.php
@@ -1,48 +1,35 @@
 <div id="sgn-pup" class="modal fade" role="dialog">
-  <div class="modal-dialog width-adj10">
-
-    {{-- Modal content--}}
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Please Sign Up</h4>
-      </div>
-      <div class="modal-body">
-
-        <form class="" action="#">
-            
-            <div class="form-group">
-            <input type="text" required placeholder="First Name" id="InputEmail1" class="form-control normal-input">
-            </div>
-
-            <div class="form-group">
-            <input type="text" required placeholder="Last Name" id="InputEmail1" class="form-control normal-input">
-            </div>
-
-            <div class="form-group">
-            <input type="email" required placeholder="Email" id="InputEmail1" class="form-control normal-input">
-            </div>
-
-            <div class="form-group">
-            <input type="password" required placeholder="Password" id="InputPasswords" class="form-control normal-input">
-            </div>
-
-            <div class="pos-adj4">
-                By signing up you accept Mapitem's <a style="color: #23a500 !important; font-size: 10pt !important; padding: 0 !important;" href="#">Policy</a> and <a style="color:#23a500 !important; font-size: 10pt !important; padding: 0 !important;" href="#">Term of use</a>
-            </div>
-
-            <button class="btn btn-default green-small3" type="submit">Sign up</button>
-
-            <div class="pos-adj3">
-                <span>Already a member ? </span> <a data-target="#lgn-pup" data-toggle="modal" style="color: #23a500 !important; font-size: 10pt !important; padding: 0 !important;" href="#" class="si">Sign in</a>
-            </div>
-
-        </form>
-
-      </div>
-      <div class="clearfix margin-twenty"></div>
-      
-    </div>
-
-  </div>
+	<div class="modal-dialog width-adj10">
+		{{-- Modal content--}}
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+				<h4 class="modal-title">Please Sign Up</h4>
+			</div>
+			<div class="modal-body">
+				<form class="" action="#">
+					<div class="form-group">
+						<input type="text" required placeholder="First Name" id="InputEmail1" class="form-control normal-input">
+					</div>
+					<div class="form-group">
+						<input type="text" required placeholder="Last Name" id="InputEmail1" class="form-control normal-input">
+					</div>
+					<div class="form-group">
+						<input type="email" required placeholder="Email" id="InputEmail1" class="form-control normal-input">
+					</div>
+					<div class="form-group">
+						<input type="password" required placeholder="Password" id="InputPasswords" class="form-control normal-input">
+					</div>
+					<div class="pos-adj4">
+						By signing up you accept Mapitem's <a style="color: #23a500 !important; font-size: 10pt !important; padding: 0 !important;" href="#">Policy</a> and <a style="color:#23a500 !important; font-size: 10pt !important; padding: 0 !important;" href="#">Term of use</a>
+					</div>
+					<button class="btn btn-default green-small3" type="submit">Sign up</button>
+					<div class="pos-adj3">
+						<span>Already a member ? </span> <a data-target="#lgn-pup" data-toggle="modal" style="color: #23a500 !important; font-size: 10pt !important; padding: 0 !important;" href="#" class="si">Sign in</a>
+					</div>
+				</form>
+			</div>
+			<div class="clearfix margin-twenty"></div>
+		</div>
+	</div>
 </div>
--- resources/views/user/listing/single_item.blade.php
+++ resources/views/user/listing/single_item.blade.php
@@ -1,22 +1,22 @@
 <div class="col-lg-4 col-sm-6">
-    <div class="pos-rel">
-      <a href="#" class="wsh-lst">@include('hearts-svg')</a>
-        <div class="box showonmap9">
-            <div class="img-box-list">
-                <img src="images/a-pic3.jpg">
-            </div>
-            <div class="box-content">
-                <h5>Iphone</h5>
-                <h6> $500</h6>
-                <div class="clearfix margin-bottom-ten"></div>
-                <img class="pull-left width-adj2" src="images/pp-5.jpg">
-                <div class="pull-left margin-left-ten width-adj3">
-                    <p class="pull-left dot1">
-                        Iphone 4s with complete accesseries for sale.
-                        Original Box is also with iPhone. It's condition is really OutStanding ...<br> 
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>    
+	<div class="pos-rel">
+		<a href="#" class="wsh-lst">@include('hearts-svg')</a>
+		<div class="box showonmap9">
+			<div class="img-box-list">
+				<img src="images/a-pic3.jpg">
+			</div>
+			<div class="box-content">
+				<h5>Iphone</h5>
+				<h6> $500</h6>
+				<div class="clearfix margin-bottom-ten"></div>
+				<img class="pull-left width-adj2" src="images/pp-5.jpg">
+				<div class="pull-left margin-left-ten width-adj3">
+					<p class="pull-left dot1">
+					Iphone 4s with complete accesseries for sale.
+					Original Box is also with iPhone. It's condition is really OutStanding ...<br> 
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>    
 </div>
