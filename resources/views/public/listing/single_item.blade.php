f6b5c14447d1b5163d8bf08d62e4f0b5717a2559
S. M. Abrar Jahin <abrarjahin@live.com>
2016-06-05 12:26:38 +0600

    Search Ietm user Descrition removed

--- resources/views/public/listing/single_item.blade.php
+++ resources/views/public/listing/single_item.blade.php
@@ -8,6 +8,7 @@
 			<div class="box-content">
 				<h5>Iphone</h5>
 				<h6> $500</h6>
+				{{--
 				<div class="clearfix margin-bottom-ten"></div>
 				<img class="pull-left width-adj2" src="images/pp-5.jpg">
 				<div class="pull-left margin-left-ten width-adj3">
@@ -16,6 +17,7 @@
 					Original Box is also with iPhone. It's condition is really OutStanding ...<br> 
 					</p>
 				</div>
+				--}}
 			</div>
 		</div>
 	</div>    
