4a02b475b2d3f415615e60f4ce4690e717dee2ee
S. M. Abrar Jahin <abrarjahin@live.com>
2016-06-09 22:17:37 +0600

    infowindow done without hearts icon

--- public/js/map.js
+++ public/js/map.js
@@ -108,7 +108,7 @@ function randomMarkers(bounds)
 																		+				'<img src="images/a-pic3.jpg">'
 																		+			'</div>'
 																		+			'<div class="box-content box-content-map">'
-																		+				'<h5>Iphone <button onclick="showDetail('+context.data.id+')" type="button" class="btn btn-info pull-right map-infowindow-detail">Detail</button></h5>'
+																		+				'<h5><div class="pull-center">Iphone</div></h5>'
 																		+				'<h6> $500</h6>'
 																		+			'</div>'
 																		+		'</div>'
@@ -140,50 +140,39 @@ function randomMarkers(bounds)
 													}
 
 													//Managing InfoWindow Contents - Fixing Contents
-													/*setTimeout(function()
+													setTimeout(function()
 													{
-														// Reference to the DIV that wraps the bottom of infowindow
+														// Reference to the DIV which receives the contents of the infowindow using jQuery
 														var iwOuter = $('.gm-style-iw');
 
-														// Since this div is in a position prior to .gm-div style-iw.
-														// We use jQuery and create a iwBackground variable,
-														// and took advantage of the existing reference .gm-style-iw for the previous div with .prev().
+														//Remove extra space behind infowindow
+														iwOuter.parent().width('20px');
+
+														/* The DIV we want to change is above the .gm-style-iw DIV.
+														* So, we use jQuery and create a iwBackground variable,
+														* and took advantage of the existing reference to .gm-style-iw for the previous DIV with .prev().
+														*/
 														var iwBackground = iwOuter.prev();
 
-														// Removes background shadow DIV
+														// Remove the background shadow DIV
 														iwBackground.children(':nth-child(2)').css({'display' : 'none'});
 
-														// Removes white background DIV
+														// Remove the white background DIV
 														iwBackground.children(':nth-child(4)').css({'display' : 'none'});
 
-														// Moves the infowindow 115px to the right.
-														iwOuter.parent().parent().css({left: '115px'});
+														//Moving the map conotent - 115 px right
+														iwOuter.parent().parent().css({left: '30px'});
 
-														// Moves the shadow of the arrow 76px to the left margin.
-														iwBackground.children(':nth-child(1)').attr('style', function(i,s){ return s + 'left: 76px !important;'});
+														// Moves the shadow of the arrow 76px to the left margin 
+														iwBackground.children(':nth-child(1)').attr('style', function(i,s){ return s + 'left: 94px !important;'});
 
-														// Moves the arrow 76px to the left margin.
-														iwBackground.children(':nth-child(3)').attr('style', function(i,s){ return s + 'left: 76px !important;'});
+														// Moves the arrow 76px to the left margin 
+														iwBackground.children(':nth-child(3)').attr('style', function(i,s){ return s + 'left: 94px !important;'});
 
 														// Changes the desired tail shadow color.
 														iwBackground.children(':nth-child(3)').find('div').children().css({'box-shadow': 'rgba(72, 181, 233, 0.6) 0px 1px 6px', 'z-index' : '1'});
-
-														// Reference to the div that groups the close button elements.
-														var iwCloseBtn = iwOuter.next();
-
-														// Apply the desired effect to the close button
-														iwCloseBtn.css({opacity: '1', right: '38px', top: '3px', border: '7px solid #48b5e9', 'border-radius': '13px', 'box-shadow': '0 0 5px #3990B9'});
-
-														// If the content of infowindow not exceed the set maximum height, then the gradient is removed.
-														if($('.iw-content').height() < 140){
-														$('.iw-bottom-gradient').css({display: 'none'});
-														}
-
-														// The API automatically applies 0.7 opacity to the button after the mouseout event. This function reverses this event to the desired value.
-														iwCloseBtn.mouseout(function(){
-														$(this).css({opacity: '1'});
-														});
-													}, 30);*/
+														
+													}, 30);
 												},
 												mouseout: function()
 												{
