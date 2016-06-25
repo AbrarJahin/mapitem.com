6f0a37114707a6d5e8721a95f58f632fc1609840
S. M. Abrar Jahin <abrarjahin@live.com>
2016-06-25 23:50:55 +0600

    Add Posting CSS not done perfectly with CDN Problem Fix

--- resources/views/css.blade.php
+++ resources/views/css.blade.php
@@ -1,17 +1,13 @@
-{{-- Bootstrap,Animate, Dropzone, FontAwesome - CSS --}}
-<link href="https://cdn.jsdelivr.net/g/bootstrap@3.3.6(css/bootstrap.min.css),animatecss@3.5.2,fontawesome@4.3.0,dropzone@4.0.1(dropzone.min.css)" />
+{{-- Should Combine this 4 CSS - Can't do that with JSDeliver --}}
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+<link rel="stylesheet" href="http://daneden.github.io/animate.css/animate.min.css" type="text/css">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.0.1/min/dropzone.min.css" rel="stylesheet" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" type="text/css">
+{{-- Should Combine this 4 CSS --}}
 
 {{-- Custom Fonts --}}
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Raleway:400,700,500' rel='stylesheet' type='text/css'>
-<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 {{-- Custom CSS --}}
 <link rel="stylesheet" href="{{ URL::asset('css/main.css') }}" type="text/css">
-{{--
-<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-<!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-<![endif]-->
---}}
