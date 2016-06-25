a275d87e34a51366aa95914be7429bc4cd86de49
S. M. Abrar Jahin <abrarjahin@live.com>
2016-06-25 23:02:28 +0600

    CSS CDN Optimization

--- resources/views/css.blade.php
+++ resources/views/css.blade.php
@@ -1,21 +1,10 @@
-{{--
-<!-- Bootstrap Core CSS -->
-<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
---}}
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-{{--
-<!-- Animate Core CSS -->
-<link rel="stylesheet" href="css/animate.css" type="text/css">
---}}
-<link rel="stylesheet" href="http://daneden.github.io/animate.css/animate.min.css" type="text/css">
+{{-- Bootstrap,Animate, Dropzone, FontAwesome - CSS --}}
+<link href="https://cdn.jsdelivr.net/g/bootstrap@3.3.6(css/bootstrap.min.css),animatecss@3.5.2,fontawesome@4.3.0,dropzone@4.0.1(dropzone.min.css)" />
+
 {{-- Custom Fonts --}}
-<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Raleway:400,700,500' rel='stylesheet' type='text/css'>
-{{-- <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" type="text/css"> --}}
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" type="text/css">
-{{-- Dropzone - CSS --}}
-<link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.0.1/min/dropzone.min.css" rel="stylesheet" />
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 {{-- Custom CSS --}}
 <link rel="stylesheet" href="{{ URL::asset('css/main.css') }}" type="text/css">
 {{--
