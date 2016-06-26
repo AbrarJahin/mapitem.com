a03c492d59775a8951a43c11bb393e271201ced5
S. M. Abrar Jahin <abrarjahin@live.com>
2016-06-26 17:55:10 +0600

    Add Edit View Not Found - Solved in my_adds page

--- resources/views/user/my_adds/inactive_add.blade.php
+++ resources/views/user/my_adds/inactive_add.blade.php
@@ -1,57 +1,57 @@
 <div class="db-body pos-rel">
-    <div class="inative-list"><a href="" class="relist">Relist</a><br><span class="ia">Inactive Ad</span></div>
-    <div class="active-list">
-        <section class="p-head col-lg-12 demo">
-            <div class="width-adj11">
-               <div class="dropdown pull-left">
-                 <button class="dropdown-toggle sbtn" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                   <i class="fa fa-pencil "></i>Settings
-                 </button>
-                 <ul class="dropdown-menu no-underline" aria-labelledby="dropdownMenu1">
-                   <li><a class="edit1" href="#">Edit</a></li>
-                   <li><a href="#">End Listing</a></li>
-                 </ul>
-               </div>
-            </div>
-           <div class="width-adj12"><i class="fa fa-heart-o"></i><span>Added to wishlist by :</span><b>5 Users</b></div>
-           <div class="width-adj13"><i class="fa fa-comment-o"></i><span>Reviews :</span><b>8</b></div>
-           <div class="width-adj14"><i class="fa fa-eye"></i><span>Viewed by :</span><b>150</b></div>
-           <div class="width-adj15"><i class="fa fa-thumbs-up"></i><span>  Liked by :</span><b>150</b></div>
-        </section>
+		<div class="inative-list"><a href="" class="relist">Relist</a><br><span class="ia">Inactive Ad</span></div>
+		<div class="active-list">
+				<section class="p-head col-lg-12 demo">
+						<div class="width-adj11">
+							<div class="dropdown pull-left">
+								<button class="dropdown-toggle sbtn" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+									 <i class="fa fa-pencil "></i>Settings
+								</button>
+								<ul class="dropdown-menu no-underline" aria-labelledby="dropdownMenu1">
+									 <li><a class="edit1" href="#">Edit</a></li>
+									 <li><a href="#">End Listing</a></li>
+								</ul>
+							</div>
+						</div>
+					<div class="width-adj12"><i class="fa fa-heart-o"></i><span>Added to wishlist by :</span><b>5 Users</b></div>
+					<div class="width-adj13"><i class="fa fa-comment-o"></i><span>Reviews :</span><b>8</b></div>
+					<div class="width-adj14"><i class="fa fa-eye"></i><span>Viewed by :</span><b>150</b></div>
+					<div class="width-adj15"><i class="fa fa-thumbs-up"></i><span>  Liked by :</span><b>150</b></div>
+				</section>
 
-        <div class="db-adc">
-            <div class="col-lg-3 col-md-3 col-sm-5 no-padding">
-                <img class="pic-adj img-responsive" src="images/db-img1.jpg">
-            </div>
+				<div class="db-adc">
+					<div class="col-lg-3 col-md-3 col-sm-5 no-padding">
+							<img class="pic-adj img-responsive" src="images/db-img1.jpg">
+					</div>
 
-        	<div class="col-lg-9 col-md-9 col-sm-7 dleft">
-            	<h3>Hill Vacations</h3>
-	            <div class="pull-right">
-	                <span>Price : </span>
-	                <b>$500</b>
-	            </div>
-	            <div class="clearfix"></div>
-	            <p>Enjoy Trip to Kashmir Valley, Kashmir Valley is the one Natural Place where Torreist comes across the world. <a href="#">View More</a></p>
+					<div class="col-lg-9 col-md-9 col-sm-7 dleft">
+						<h3>Hill Vacations</h3>
+						<div class="pull-right">
+								<span>Price : </span>
+								<b>$500</b>
+						</div>
+						<div class="clearfix"></div>
+						<p>Enjoy Trip to Kashmir Valley, Kashmir Valley is the one Natural Place where Torreist comes across the world. <a href="#">View More</a></p>
 
-            	<ul>
-	                <li>
-	                    <span><img src="images/u-logo.jpg"></span>
-	                    Paradise Travelling Company
-	                </li>
-	                <li>
-	                    <span class="fa fa-fw icon-adj"> </span>
-	                    123-456-789
-	                </li>
-	                <li>
-	                    <span class="fa fa-fw icon-adj"> </span>
-	                    <a href="">www.paradisetravel.com</a>
-	                </li>
-                	<li>
-	                    <span class="fa fa-fw icon-adj"> </span>
-	                    <a href="mailto:someone@example.com?Subject=Hello%20again" target="_top">info@pardistravel.com</a>
-                	</li>
-            	</ul>
-       	 	</div>
-    	</div>
+						<ul>
+								<li>
+										<span><img src="images/u-logo.jpg"></span>
+										Paradise Travelling Company
+								</li>
+								<li>
+										<span class="fa fa-fw icon-adj"> </span>
+										123-456-789
+								</li>
+								<li>
+										<span class="fa fa-fw icon-adj"> </span>
+										<a href="">www.paradisetravel.com</a>
+								</li>
+								<li>
+										<span class="fa fa-fw icon-adj"> </span>
+										<a href="mailto:someone@example.com?Subject=Hello%20again" target="_top">info@pardistravel.com</a>
+								</li>
+						</ul>
+					</div>
+			</div>
 	</div>
 </div>
--- resources/views/user/my_adds/main.blade.php
+++ resources/views/user/my_adds/main.blade.php
@@ -5,8 +5,9 @@
 @section('meta_author', 'S. M. Abrar Jahin')
 
 @section('content')
-	@for ($i = 0; $i < 5; $i++)
+	@for ($i = 0; $i < 1; $i++)
 	    @include('user.my_adds.active_add')
 	    @include('user.my_adds.inactive_add')
 	@endfor
+	@include('user.my_adds.add_edit')
 @endsection
