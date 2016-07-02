44da5f313e6a083bfa12254e14faf30fe1d53774
S. M. Abrar Jahin <abrarjahin@live.com>
2016-07-02 14:11:25 +0600

    Image Scroller Fixing done in listing page

--- resources/views/public/listing/add_detail.blade.php
+++ resources/views/public/listing/add_detail.blade.php
@@ -1,185 +1,202 @@
 <div class="results-det">
-    <div class="col-lg-9">
-        <div class="col-lg-12 no-padding rd-top-row">
-            <h4 class="pull-left">Iphone</h4>
-            <a class="direction pull-right" href="#"><i class="fa fa-location-arrow"></i>Directions</a>
-        </div>
-        <div class="clearfix"></div>
-        <div class="col-lg-12 no-padding rd-top-row2">
-            <h5 class="pull-left">Price: <span>$1000</span></h5>
-            <div class="pull-right popups">
-                <a data-original-title="Add to Wishlist" href="#" class="wsh-lst-adpage grey-tooltip" data-toggle="tooltip" data-placement="top">
-                    <object type="image/svg+xml" width="14px" data="{{ URL::asset('svg/normal.svg') }}"></object>
-                </a>
-                <a data-placement="top" data-toggle="tooltip" class="fa fa-comment-o grey-tooltip" href="#" data-original-title="Reviews"></a>
+	<div class="col-lg-9">
+		<div class="col-lg-12 no-padding rd-top-row">
+			<h4 class="pull-left">Iphone</h4>
+			<a class="direction pull-right" href="#"><i class="fa fa-location-arrow"></i>Directions</a>
+		</div>
 
-                <a data-placement="top" data-toggle="tooltip" class="fa fa-eye grey-tooltip" href="#" data-original-title="157"></a>
+		<div class="clearfix"></div>
 
-                <a data-placement="top" data-toggle="tooltip" class="fa fa-thumbs-o-up grey-tooltip" href="#" data-original-title="Like"></a>
-            </div>
-        </div>
-        <div class="clearfix"></div>
-        <div class="col-lg-12 no-padding">
-            <div class="dropdown">
-                <a data-toggle="dropdown" class="direction dropdown-toggle loginbtn pull-left" href="#"><i class="fa fa-gavel"></i>Send Offer
-                </a>
+		<div class="col-lg-12 no-padding rd-top-row2">
+			<h5 class="pull-left">Price: <span>$1000</span></h5>
+			<div class="pull-right popups">
+				<a data-original-title="Add to Wishlist" href="#" class="wsh-lst-adpage grey-tooltip" data-toggle="tooltip" data-placement="top">
+					<object type="image/svg+xml" width="14px" data="{{ URL::asset('svg/normal.svg') }}"></object>
+				</a>
+				<a data-placement="top" data-toggle="tooltip" class="fa fa-comment-o grey-tooltip" href="#" data-original-title="Reviews"></a>
 
-                    <!-- If user is logged in  -->
-                    <!-- <ul class="dropdown-menu no-padding loginpopup col-lg-4">
-                        <li>
-                            <form class="offer" action="#">
-                                <div class="form-group">
-                                    <span class="text-adj1">Name : Jonathan Kaneer </span>
-                                </div>
-                                <div class="form-group">
-                                <span class="text-adj1">Email : jk@yahoo.com </span>
-                                </div>
-                                <div class="form-group">
-                                <span class="text-adj1">Cell : 123-456-789 </span>
-                                </div>
-                                <div class="form-group">
-                                    <input type="text" onkeypress="return numbersonly(this, event)" placeholder="Your Offer in $" id="InputPasswords" class="form-control normal-input">
-                                </div>
-                                <div class="form-group">
-                                    <textarea class="form-control medium-textarea no-margin" rows="3" placeholder="Message"></textarea>
-                                </div>
-                                <button class="btn btn-default green-small" type="submit">Place Offer</button>
-                            </form>
+				<a data-placement="top" data-toggle="tooltip" class="fa fa-eye grey-tooltip" href="#" data-original-title="157"></a>
 
-                        </li>
-                        
-                    </ul> -->
-                    <!-- If user is not logged in  -->
+				<a data-placement="top" data-toggle="tooltip" class="fa fa-thumbs-o-up grey-tooltip" href="#" data-original-title="Like"></a>
+			</div>
+		</div>
 
-                <ul class="dropdown-menu">
-                    <li>
-                        <div class="pos-adj03">
-                            <span>Please </span><a href="#" class="def" data-toggle="modal" data-target="#lgn-pup" href="#" style="color: #23a500 !important; font-size: 10pt !important; padding: 0 !important;">LOGIN</a>
-                            or
-                            <a data-toggle="modal" data-target="#sgn-pup" class="" href="#" style="color: #23a500 !important; font-size: 10pt !important; padding: 0 !important;">SIGN UP</a> to continue
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="clearfix"></div>
-        <div class="col-lg-12 no-padding description rd-top-row">
-          
+		<div class="clearfix"></div>
 
-             - Complet accesseries are available<br>
-             - Out Standing Condition.<br>
-             - 2 months warenty remaining <br><br>
+		<div class="col-lg-12 no-padding">
+			<div class="dropdown">
+				<a data-toggle="dropdown" class="direction dropdown-toggle loginbtn pull-left" href="#"><i class="fa fa-gavel"></i>Send Offer
+				</a>
 
-            Iphone 4s with complete accesseries for sale. Original Box is also with iPhone Iphone 4s with complete 
-            accesseries for sale. Original Box is also with iPhone. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, 
-            sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.
+				{{-- If user is logged in --}}
+				@if (Auth::check())
+					<ul class="dropdown-menu no-padding loginpopup col-lg-4">
+						<li>
+							<form class="offer" action="#">
+								<div class="form-group">
+									<span class="text-adj1">Name : Jonathan Kaneer </span>
+								</div>
+								<div class="form-group">
+								<span class="text-adj1">Email : jk@yahoo.com </span>
+								</div>
+								<div class="form-group">
+								<span class="text-adj1">Cell : 123-456-789 </span>
+								</div>
+								<div class="form-group">
+									<input type="text" onkeypress="return numbersonly(this, event)" placeholder="Your Offer in $" id="InputPasswords" class="form-control normal-input">
+								</div>
+								<div class="form-group">
+									<textarea class="form-control medium-textarea no-margin" rows="3" placeholder="Message"></textarea>
+								</div>
+								<button class="btn btn-default green-small" type="submit">Place Offer</button>
+							</form>
 
-            <div class="cleafix"></div>
+						</li>
+						
+					</ul>
+				@else	{{-- If user is not logged in --}}
+					<ul class="dropdown-menu">
+						<li>
+							<div class="pos-adj03">
+								<span>Please </span><a href="#" class="def" data-toggle="modal" data-target="#lgn-pup" href="#" style="color: #23a500 !important; font-size: 10pt !important; padding: 0 !important;">LOGIN</a>
+								or
+								<a data-toggle="modal" data-target="#sgn-pup" class="" href="#" style="color: #23a500 !important; font-size: 10pt !important; padding: 0 !important;">SIGN UP</a> to continue
+							</div>
+						</li>
+					</ul>
+				@endif
+			</div>
+		</div>
 
-            <a class="review" href="#">Write Review</a>
+		<div class="clearfix"></div>
+		<div class="col-lg-12 no-padding description rd-top-row">
+		  
 
-            <div class="write-review hide">
-                <form action="#" class="offer">
-                    <div class="form-group">
-                        <textarea class="form-control tarea" placeholder="Write your Reviews" rows="3"></textarea>
-                        <div class="col-lg-12 margin-top-twenty margin-bottom-twenty">
-                         <span class="pull-left margin-right-twenty">Please rate your experience</span>
-                        <input type="number" class="rating" id="test" name="test" data-min="1" data-max="5" value="0">
-                        </div>
+			 - Complet accesseries are available<br>
+			 - Out Standing Condition.<br>
+			 - 2 months warenty remaining <br><br>
 
-                    </div>
+			Iphone 4s with complete accesseries for sale. Original Box is also with iPhone Iphone 4s with complete 
+			accesseries for sale. Original Box is also with iPhone. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, 
+			sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.
 
-                    <button type="submit" class="btn btn-default green-small review-submit show">Submit</button>
-                </form>
-            </div>
+			<div class="cleafix"></div>
 
-            <div class="share">
+			<a class="review" href="#">Write Review</a>
 
-                <span>Share on </span>
+			<div class="write-review hide">
+				<form action="#" class="offer">
+					<div class="form-group">
+						<textarea class="form-control tarea" placeholder="Write your Reviews" rows="3"></textarea>
+						<div class="col-lg-12 margin-top-twenty margin-bottom-twenty">
+						 <span class="pull-left margin-right-twenty">Please rate your experience</span>
+						<input type="number" class="rating" id="test" name="test" data-min="1" data-max="5" value="0">
+						</div>
 
-                <a class="sm fa fa-facebook " href="#"></a>
-                <a class="sm fa fa-twitter " href="#"></a>
-                <a class="sm fa  fa-google-plus" href="#"></a>
+					</div>
 
-            </div>
+					<button type="submit" class="btn btn-default green-small review-submit show">Submit</button>
+				</form>
+			</div>
 
-        </div>
+			<div class="share">
 
-        <div class="col-lg-12 reviews">
+				<span>Share on </span>
 
-            <div class="col-lg-4 rone">
-              Jonathan Kaneer<br>
-                <i class="fa fa-star fa-xs green-text"></i>
-                <i class="fa fa-star fa-xs green-text"></i>
-                <i class="fa fa-star-o fa-xs"></i>
-                <i class="fa fa-star-o fa-xs"></i>
-                <i class="fa fa-star-o fa-xs"></i><br>
-              <span>May 5, 2015</span>
+				<a class="sm fa fa-facebook " href="#"></a>
+				<a class="sm fa fa-twitter " href="#"></a>
+				<a class="sm fa  fa-google-plus" href="#"></a>
 
-            </div>
+			</div>
 
-            <div class="col-lg-8 rtwo">
-              <span>Everything seems good as far as it’s the same as written</span>
-            </div>
+		</div>
 
-            <div class="clearfix margin-twenty"></div>
+		<div class="col-lg-12 reviews">
 
-            <div class="col-lg-4 rone">
-              Bilal Munawar<br>
-                <i class="fa fa-star fa-xs green-text"></i>
-                <i class="fa fa-star-o fa-xs"></i>
-                <i class="fa fa-star-o fa-xs"></i>
-                <i class="fa fa-star-o fa-xs"></i>
-                <i class="fa fa-star-o fa-xs"></i><br>
-              <span>June 5, 2015</span>
-            </div>
+			<div class="col-lg-4 rone">
+			  Jonathan Kaneer<br>
+				<i class="fa fa-star fa-xs green-text"></i>
+				<i class="fa fa-star fa-xs green-text"></i>
+				<i class="fa fa-star-o fa-xs"></i>
+				<i class="fa fa-star-o fa-xs"></i>
+				<i class="fa fa-star-o fa-xs"></i><br>
+			  <span>May 5, 2015</span>
 
-            <div class="col-lg-8 rtwo">
-              <span>I wish to have this phone in less price I have contacted but she did not accept my offer</span>
-            </div>
+			</div>
 
-            <div class="clearfix margin-twenty"></div>
+			<div class="col-lg-8 rtwo">
+			  <span>Everything seems good as far as it’s the same as written</span>
+			</div>
 
-        </div>
+			<div class="clearfix margin-twenty"></div>
 
-    </div>
+			<div class="col-lg-4 rone">
+			  Bilal Munawar<br>
+				<i class="fa fa-star fa-xs green-text"></i>
+				<i class="fa fa-star-o fa-xs"></i>
+				<i class="fa fa-star-o fa-xs"></i>
+				<i class="fa fa-star-o fa-xs"></i>
+				<i class="fa fa-star-o fa-xs"></i><br>
+			  <span>June 5, 2015</span>
+			</div>
 
-    <div class="col-lg-3 no-padding">
+			<div class="col-lg-8 rtwo">
+			  <span>I wish to have this phone in less price I have contacted but she did not accept my offer</span>
+			</div>
 
-        <div class="contact-bar">
-          
-            <div class="sb-top">
-                <img class="pround" alt="Jesica" src="{{ URL::asset('images/pp-1.jpg') }}">
-                <h6>Jesica Alben</h6>
-                <div class="star">
-                    <i class="fa fa-star green-text"></i>
-                    <i class="fa fa-star green-text"></i>
-                    <i class="fa fa-star green-text"></i>
-                    <i class="fa fa-star-o"></i>
-                    <i class="fa fa-star-o"></i>
-                </div>
-            </div>
+			<div class="clearfix margin-twenty"></div>
 
-            <div class="sb-middle">
-                <span><i class="fa fa-phone"></i>123-456-789</span>
-                <span><i class="fa fa-globe"></i><a #="" href="">www.jalben.com</a></span>
-                <span><i class="fa fa-envelope"></i>info@jablen.com</span>
-                <span><i class="fa fa-facebook"></i><i class="fa fa-check-circle p-adj"></i>Facebook Verified</span>
-                <span><i class="fa fa-credit-card"></i>Accepts Credit Card</span>
-            </div>
-            <div class="sb-bottom dropdown">
-                <a data-toggle="dropdown" type="submit" class="btn loginbtn green-large width-adj dropdown-toggl" href="#">Contact</a>
-                <ul class="dropdown-menu no-padding loginpopup col-lg-4 contact">
-                    <li>
-                        <form class="offer" action="#">
-                            <div class="form-group">
-                            <textarea rows="3" placeholder="Any Question" class="form-control tarea"></textarea>
-                            </div>
-                            <button class="btn btn-default green-small width-adj" type="submit">Send Message</button>
-                        </form>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
+		</div>
+
+	</div>
+
+	<div class="col-lg-3 no-padding">
+
+		<div class="contact-bar">
+		  
+			<div class="sb-top">
+				<img class="pround" alt="Jesica" src="{{ URL::asset('images/pp-1.jpg') }}">
+				<h6>Jesica Alben</h6>
+				<div class="star">
+					<i class="fa fa-star green-text"></i>
+					<i class="fa fa-star green-text"></i>
+					<i class="fa fa-star green-text"></i>
+					<i class="fa fa-star-o"></i>
+					<i class="fa fa-star-o"></i>
+				</div>
+			</div>
+
+			<div class="sb-middle">
+				<span>
+					<i class="fa fa-phone"></i>123-456-789
+				</span>
+				<span>
+					<i class="fa fa-globe"></i><a #="" href="">www.jalben.com</a>
+				</span>
+				<span>
+					<i class="fa fa-envelope"></i>info@jablen.com
+				</span>
+				<span>
+					<i class="fa fa-facebook"></i><i class="fa fa-check-circle p-adj"></i>Facebook Verified
+				</span>
+				<span>
+					<i class="fa fa-credit-card"></i>Accepts Credit Card
+				</span>
+			</div>
+
+			<div class="sb-bottom dropdown">
+				<a data-toggle="dropdown" type="submit" class="btn loginbtn green-large width-adj dropdown-toggl" href="#">Contact</a>
+				<ul class="dropdown-menu no-padding loginpopup col-lg-4 contact">
+					<li>
+						<form class="offer" action="#">
+							<div class="form-group">
+							<textarea rows="3" placeholder="Any Question" class="form-control tarea"></textarea>
+							</div>
+							<button class="btn btn-default green-small width-adj" type="submit">Send Message</button>
+						</form>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </div>
