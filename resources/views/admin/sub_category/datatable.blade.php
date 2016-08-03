c139c1e516942a375fc33d6430b32896cec032e0
S. M. Abrar Jahin <abrarjahin@live.com>
2016-08-03 08:20:57 +0600

    Design Updated in Admin Tables

--- resources/views/admin/sub_category/datatable.blade.php
+++ resources/views/admin/sub_category/datatable.blade.php
@@ -1,7 +1,7 @@
 <meta name="datatable_ajax_url" content="{{ URL::route('admin.sub_category_datable') }}">
 
 {{-- Category Datatable --}}
-<table id="sub-category-datatable"  cellpadding="0" cellspacing="0" border="0" class="display" width="100%">
+<table id="sub-category-datatable" class="table table-hover" cellpadding="0" cellspacing="0" border="0" class="display" width="100%">
 		<thead>
 			<tr>
 				<th>Category Name</th>
--- resources/views/admin/users/datatable.blade.php
+++ resources/views/admin/users/datatable.blade.php
@@ -1,7 +1,7 @@
 <meta name="datatable_ajax_url" content="{{ URL::route('admin.users_datable') }}">
 
 {{-- Category Datatable --}}
-<table id="user-datatable" cellpadding="0" cellspacing="0" border="0" class="display" width="100%">
+<table id="user-datatable" class="table table-hover" cellpadding="0" cellspacing="0" border="0" class="display" width="100%">
 		<thead>
 			<tr>
 				<th>Full Name</th>
