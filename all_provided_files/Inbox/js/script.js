d6ce8113f1ed88c0854cbf0acd9bdedaec52d44b
S. M. Abrar Jahin <abrarjahin@live.com>
2016-08-19 09:35:20 +0600

    Inbox design Updated

--- all_provided_files/Inbox/js/script.js
+++ all_provided_files/Inbox/js/script.js
@@ -1,7 +1,37 @@
+function showMessageDetail()
+{
+	if(window.matchMedia( "(max-width: 768px)" ).matches)
+	{
+		$('#message_details').hide();
+		//$('#message_menu_close_button').show();
+	}
+	else
+	{
+		$('#message_details').show();
+		//$('#message_menu_close_button').hide();
+	}
+}
+
+$(window).resize(function()
+{
+	showMessageDetail();
+});
+
 $(document).ready(function()
 {
-    $('.pro_pic').each(function()
-    {
-		$(this).height($(this).parent().height());
-    });
+	showMessageDetail();
+
+	$('.inbox_titles li').click(function(event)
+	{
+		if(window.matchMedia( "(max-width: 768px)" ).matches)
+		{
+			$('#message_menu_close_button').show();
+			$('#message_details').show();
+			
+		}
+	});
+	$('#message_menu_close_button').click(function(event)
+	{
+		alert('Clicked');
+	});
 });
