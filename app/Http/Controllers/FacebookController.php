67fade05e85b24f9a33d4736698e3eaeaf78ac7b
S. M. Abrar Jahin <abrarjahin@live.com>
2016-08-30 09:35:44 +0600

    FB Login Debug

--- app/Http/Controllers/FacebookController.php
+++ app/Http/Controllers/FacebookController.php
@@ -46,6 +46,6 @@ class FacebookController extends Controller
         $user = Socialite::driver('facebook')->user();
 
         // $user->token;
-        return $user;
+        var_dump($user);
     }
 }
