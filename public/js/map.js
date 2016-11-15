b89e0f668fad84223ecfaa882a69ec328e118ee0
S. M. Abrar Jahin <abrarjahin@live.com>
2016-11-15 08:01:08 +0600

    Force fix (Bangla) for scroll issue in map popup

--- public/js/map.js
+++ public/js/map.js
@@ -840,6 +840,20 @@ function fixInfowindowScroll()
 		$(".map-master-div").parent().parent().attr("style", "overflow: hidden; cursor: default; clear: both; position: relative; padding: 0px; background-color: rgb(255, 255, 255); border-color: rgb(204, 204, 204); border-style: solid; border-radius: 10px; border-width: 1px; width: 300px; height: 227px;");
 		//$(".map-master-div").parent().parent().css("overflow", "hidden");
 		$(".map-master-div").css("width", "100%");
+	}, 100);
+
+	setTimeout(function()
+	{
+		$(".map-master-div").parent().parent().attr("style", "overflow: hidden; cursor: default; clear: both; position: relative; padding: 0px; background-color: rgb(255, 255, 255); border-color: rgb(204, 204, 204); border-style: solid; border-radius: 10px; border-width: 1px; width: 300px; height: 227px;");
+		//$(".map-master-div").parent().parent().css("overflow", "hidden");
+		$(".map-master-div").css("width", "100%");
+	}, 200);
+
+	setTimeout(function()
+	{
+		$(".map-master-div").parent().parent().attr("style", "overflow: hidden; cursor: default; clear: both; position: relative; padding: 0px; background-color: rgb(255, 255, 255); border-color: rgb(204, 204, 204); border-style: solid; border-radius: 10px; border-width: 1px; width: 300px; height: 227px;");
+		//$(".map-master-div").parent().parent().css("overflow", "hidden");
+		$(".map-master-div").css("width", "100%");
 	}, 400);
 }
 
