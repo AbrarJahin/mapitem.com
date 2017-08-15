1147bcacb1552be75ca147fc1fa54422cb8c2673
Bilal Munawar <bilal5663@gmail.com>
2017-08-15 10:44:29 +0500

    nf

--- resources/views/public/listing/navigation.blade.php
+++ resources/views/public/listing/navigation.blade.php
@@ -1,23 +1,22 @@
-<nav class="navbar navbar-inverse navbar-fixed-top ip-adj2 cbp-spmenu-push" role="navigation">
+<nav class="navbar navbar-inverse navbar-fixed-top ip-adj2" role="navigation">
 	<div class="container-fluid">
 		{{-- Brand and toggle get grouped for better mobile display --}}
 		<div class="navbar-header">
-			<button id="showRightPush" type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
 				<span class="sr-only">Toggle navigation</span>
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
 			<a class="navbar-brand ipl" href="{{ URL::route('index') }}"><img src="{{ URL::asset('images/blockhunt-logo-minified.png') }}"></a>
-            <a href="#" class="nf-placeholder">Search By Category</a>
 		</div>
 		{{-- Collect the nav links, forms, and other content for toggling --}}
-        
-        
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
 			@include('nav_filters')
+
 			<div class="clearfix visible-xs-block"></div>
-            <div class="cbp-spmenu cbp-spmenu-vertical cbp-spmenu-right" id="cbp-spmenu-s2">
+            
                 <ul class="nav navbar-nav navbar-right ip-nav">
                     @if (Auth::check())
                         <li class="dropdown">
@@ -37,7 +36,7 @@
                         <a href="#" class="def" data-toggle="modal" data-target="@if (Auth::check())#pfa @else #lgn-pup @endif">Post free ad</a>
                     </li>
                 </ul>
-             </div>
+
 		</div>
 		{{-- /.navbar-collapse --}}
 	</div>
