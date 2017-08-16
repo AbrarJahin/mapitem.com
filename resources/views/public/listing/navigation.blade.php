3df648a29c49fa8825e23cb6223cc6230b7193a7
Bilal Munawar <bilal5663@gmail.com>
2017-08-16 14:47:48 +0500

    nf

--- resources/views/public/listing/navigation.blade.php
+++ resources/views/public/listing/navigation.blade.php
@@ -14,7 +14,7 @@
 		{{-- Collect the nav links, forms, and other content for toggling --}}
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
-			@include('nav_filters')
+			<div class="nf-hide">@include('nav_filters')</div>
 
 			<div class="clearfix visible-xs-block"></div>
             
