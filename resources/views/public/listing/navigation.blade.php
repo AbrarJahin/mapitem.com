905c3a92bb0b406c836c5786eebed96658f4f40f
Bilal Munawar <bilal5663@gmail.com>
2017-08-16 15:11:01 +0500

    nf

--- resources/views/public/listing/navigation.blade.php
+++ resources/views/public/listing/navigation.blade.php
@@ -2,16 +2,16 @@
 	<div class="container-fluid">
 		{{-- Brand and toggle get grouped for better mobile display --}}
 		<div class="navbar-header">
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" id="showRightPush">
 				<span class="sr-only">Toggle navigation</span>
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
 			<a class="navbar-brand ipl" href="{{ URL::route('index') }}"><img src="{{ URL::asset('images/blockhunt-logo-minified.png') }}"></a>
-            
+            <a href="#" class="nf-placeholder">Search By Category</a>
 		</div>
-      
+        <div class="nf-hide"><a class="fa fa-close nf-close"></a>@include('nav_filters')</div>
 		{{-- Collect the nav links, forms, and other content for toggling --}}
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
