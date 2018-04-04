479343a704b9c5d5a5e6c5e9b089408f3da667fc
Bilal Munawar <bilal5663@gmail.com>
2018-04-04 13:03:51 +0500

    new-slider 9

--- resources/views/public/listing/main.blade.php
+++ resources/views/public/listing/main.blade.php
@@ -28,6 +28,20 @@
 		</div>
 		<div class="clearfix "></div>
 		{{-- ad detail --}}
+        <div class="swiper-container">
+		<div class="swiper-wrapper">
+			<div class="swiper-slide"><img src="https://bilal.mapitem.com/public/uploads/16-1ae79834c71878780678.jpg" /></div>
+			<div class="swiper-slide"><img src="https://bilal.mapitem.com/public/uploads/16-18d97e3f3b74930ea404.jpg" /></div>
+			<div class="swiper-slide"><img src="https://bilal.mapitem.com/public/uploads/16-b16ca689d45ab214e83d.jpg" /></div>
+			<div class="swiper-slide"><img src="https://bilal.mapitem.com/public/uploads/16-bc6926843de7108b48c8.jpg" /></div>
+
+		</div>
+		<!-- Add Pagination -->
+		<div class="swiper-pagination"></div>
+		<!-- Add Arrows -->
+		<div class="swiper-button-next"></div>
+		<div class="swiper-button-prev"></div>
+</div>
 		<div class="ad-detail">
 			{{-- ad Slider--}}
 			@include('public.listing.slider_images')
